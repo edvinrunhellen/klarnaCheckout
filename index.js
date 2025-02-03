4cdf86508b7744c3cf41c8469a9bf2e69ef178a9
Eddan <edvin.runhellen1@gmail.com>
2025-02-03 10:17:48 +0100

    Added API calls for GET

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('Hello world');
+
+import {getProducts, getProduct} from './services/api.js'
+import express from "express"
+const app = express();
+import { config } from "dotenv"
+config()
+
+app.get('/', async (req, res) => {
+	const products = await getProducts();
+	const markup = products
+		.map(
+			(p) =>
+				`<a style="display:block;color:black;border:solid black 2px;margin: 20px; padding:10px;" href="/product/${p.id}">
+                    ${p.title} - ${p.price}kr
+                </a>`
+		)
+		.join(' ');
+	res.send(markup);
+});
+
+app.get('/product/:id', async function (req, res) {
+    try {
+        const { id } = req.params;
+    const product = await getProduct(id)
+	const markup = `<h1>${product.title} ${product.price}</h1>`;
+	res.send(markup);
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+app.get('/confirmation', async (req, res) => {
+    const markup = `<h1>Hello confirmation</h1>`;
+    res.send(markup);
+})
+
+
+app.listen(process.env.PORT)
